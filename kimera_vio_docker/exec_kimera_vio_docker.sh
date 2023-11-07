9b5a04aaf79575d78becb8e375c1b0a6c536f95e
Allen Frank Cutinha <allencutinha@gmail.com>
2023-11-07 13:54:10 +0100

    Update exec_kimera_vio_docker.sh

--- kimera_vio_docker/exec_kimera_vio_docker.sh
+++ kimera_vio_docker/exec_kimera_vio_docker.sh
@@ -1,1 +1,1 @@
-docker exec -it vio-docker-conda bash -c cd /root/Kimera-VIO/ && ./scripts/stereoVIOEuroc.bash -p /root/euroc/ -r
+docker exec -it vio-docker bash -c cd /root/Kimera-VIO/ && ./scripts/stereoVIOEuroc.bash -p /root/euroc/ -r
